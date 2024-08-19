b25b9cf3e0fdb6a4e163a373ba9f91d2978d196c
AnikAida <anikaidanuraeni@gmail.com>
2024-08-19 13:33:36 +0700

    pertemuan 2
    latihan2 PBO

---
+++ latihan2.16.php
@@ -0,0 +1,31 @@
+<?php
+$x =100;
+$y =50;
+
+if ($x>=100 and $y == 50){
+    echo"hello word and";
+
+}
+echo "<br>";
+
+if ($x==100 and $y == 80){
+    echo"hello word" or;
+}
+echo "<br>";
+
+if ($x==100 && $y == 50){
+    echo"hello word &&";
+}
+echo "<br>";
+
+if ($x==100 || $y == 80){
+    echo"hello word ||";
+}
+echo "<br>";
+
+if ($ !==100){
+    echo"hello word NOT";
+}
+echo "<br>";
+?>
+
